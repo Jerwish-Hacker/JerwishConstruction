7f81f25a72dfbcfe59a23818d4687c9fb5fd56e1
Justin <justinraj958@gmail.com>
2023-02-25 18:26:27 +0530

    Design page was Added by Justin

--- code.js
+++ code.js
@@ -1,19 +1,5 @@
-let color
-window.addEventListener("load", startup, false);
-let r = document.querySelector(':root');  
-let rs = getComputedStyle(r);
-
-function startup() {
-    color = document.querySelector("#color");        
-    color.addEventListener("input", updateFirst, false);
-    color.addEventListener("change", updateAll, false);        
-  }
-
-
-function updateFirst(event) {  
-  r.style.setProperty('--banner-color',event.target.value);  
-}
-  
-function updateAll(event) {    
-    r.style.setProperty('--banner-color',event.target.value);    
-}
+var myModal = document.getElementById('myModal')
+        var myInput = document.getElementById('myInput')
+        myModal.addEventListener('shown.bs.modal', function () {
+        myInput.focus()
+        })
